4ae837085c41396c1a17bb54407c552185b3f680
ramuf <48536005+ramuf@users.noreply.github.com>
2020-10-03 15:40:05 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,25 @@
-import pandas as pd
-import plotly.express as px  # (version 4.7.0)
-# import plotly.graph_objects as go
+import os
+from random import randint
 
-import dash  # (version 1.12.0) pip install dash
+import plotly.plotly as py
+from plotly.graph_objs import *
+
+import flask
+import dash
+from dash.dependencies import Input, Output, State, Event
 import dash_core_components as dcc
 import dash_html_components as html
-from dash.dependencies import Input, Output
 
-app = dash.Dash(__name__)
 
+# Setup the app
+# Make sure not to change this file name or the variable names below,
+# the template is configured to execute 'server' on 'app.py'
+server = flask.Flask(__name__)
+server.secret_key = os.environ.get('secret_key', str(randint(0, 1000000)))
+app = dash.Dash(__name__, server=server)
+
+
+# Put your Dash code here
 df = pd.read_csv("covid-19-allegheny.csv")
 
 fig_total_cases = px.line(df, x=df.date, y=df.cases, title='Total Cases')
@@ -22,5 +33,7 @@ app.layout = html.Div([
 
 ])
 
+# Run the Dash app
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.server.run(debug=True, threaded=True)
+    

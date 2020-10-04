f05343b6a0f9c8de9e135d2a90a7caf4bd31b251
ramuf <48536005+ramuf@users.noreply.github.com>
2020-10-04 15:54:00 +0100

    nothing

--- app.py
+++ app.py
@@ -42,53 +42,59 @@ layout = Layout(
     plot_bgcolor='#403e3d',
     font_color="white",
     margin=dict(pad=10),
-    colorway=['red']
+    # colorway=['red'],
+    # hovermode='x',
+    hoverlabel = dict(
+        bgcolor="#403e3d",
+        # font_size=16,
+        # font_family="Rockwell"
+    )
 )
 
 fig_new_cases_absolute = px.line(df, x='date', y='new_cases', labels={'date':'Date', 'new_cases':'New Cases'}, title='New Cases (Absolute)').update_layout(layout)
 fig_new_cases_absolute.update_xaxes(showgrid=True, gridwidth=1, gridcolor=grid_color)
 fig_new_cases_absolute.update_yaxes(showgrid=True, gridwidth=1, gridcolor=grid_color)
-fig_new_cases_absolute.update_traces(line_color=line_color)
+fig_new_cases_absolute.update_traces(line_color=line_color, hovertemplate='%{x} <br><br>New Cases: %{y}')
 
 fig_new_deaths = px.line(df, x='date', y='new_deaths', labels={'date':'Date', 'new_deaths':'New Deaths'}, title='New Deaths').update_layout(layout)
 fig_new_deaths.update_xaxes(showgrid=True, gridwidth=1, gridcolor=grid_color)
 fig_new_deaths.update_yaxes(showgrid=True, gridwidth=1, gridcolor=grid_color)
-fig_new_deaths.update_traces(line_color=line_color)
+fig_new_deaths.update_traces(line_color=line_color, hovertemplate='%{x} <br><br>New Deaths: %{y}')
 
 fig_total_cases = px.line(df, x='date', y='cases', labels={'date':'Date', 'cases':'Total Cases'}, title='Total Cases').update_layout(layout)
 fig_total_cases.update_xaxes(showgrid=True, gridwidth=1, gridcolor=grid_color)
 fig_total_cases.update_yaxes(showgrid=True, gridwidth=1, gridcolor=grid_color)
-fig_total_cases.update_traces(line_color=line_color)
+fig_total_cases.update_traces(line_color=line_color, hovertemplate='%{x} <br><br>Total Cases: %{y}')
 
 fig_total_deaths = px.line(df, x='date', y='deaths', labels={'date':'Date', 'deaths':'Total Deaths'}, title='Total Deaths').update_layout(layout)
 fig_total_deaths.update_xaxes(showgrid=True, gridwidth=1, gridcolor=grid_color)
 fig_total_deaths.update_yaxes(showgrid=True, gridwidth=1, gridcolor=grid_color)
-fig_total_deaths.update_traces(line_color=line_color)
+fig_total_deaths.update_traces(line_color=line_color, hovertemplate='%{x} <br><br>Total Deaths: %{y}')
 
 fig_fatality = px.line(df, x='date', y='cases_fatality', labels={'date':'Date', 'cases_fatality':'Fatality %'}, title='Fatality % (Total Deaths divided by Total Cases)').update_layout(layout)
 fig_fatality.update_xaxes(showgrid=True, gridwidth=1, gridcolor=grid_color)
 fig_fatality.update_yaxes(showgrid=True, gridwidth=1, gridcolor=grid_color)
-fig_fatality.update_traces(line_color=line_color)
+fig_fatality.update_traces(line_color=line_color, hovertemplate='%{x} <br><br>Fatality: %{y}%')
 
 fig_deaths_poll = px.line(df, x='date', y='deaths_poll', labels={'date':'Date', 'deaths_poll':'Deaths per 100k'}, title='Deaths per 100k population').update_layout(layout)
 fig_deaths_poll.update_xaxes(showgrid=True, gridwidth=1, gridcolor=grid_color)
 fig_deaths_poll.update_yaxes(showgrid=True, gridwidth=1, gridcolor=grid_color)
-fig_deaths_poll.update_traces(line_color=line_color)
+fig_deaths_poll.update_traces(line_color=line_color, hovertemplate='%{x} <br><br>Deaths per 100k: %{y}')
 
 fig_new_cases_smoothed = px.line(df, x='date', y='new_cases_smoothed', labels={'date':'Date', 'new_cases_smoothed':'New Cases Smoothed'}, title='New Cases Smoothed (7 Days average)').update_layout(layout)
 fig_new_cases_smoothed.update_xaxes(showgrid=True, gridwidth=1, gridcolor=grid_color)
 fig_new_cases_smoothed.update_yaxes(showgrid=True, gridwidth=1, gridcolor=grid_color)
-fig_new_cases_smoothed.update_traces(line_color=line_color)
+fig_new_cases_smoothed.update_traces(line_color=line_color, hovertemplate='%{x} <br><br>New Cases Smoothed: %{y}')
 
 fig_new_cases_incidence = px.line(df, x='date', y='daily_incidence', labels={'date':'Date', 'daily_incidence':'Daily Incidence Rate'}, title='Daily Incidence Rate (per 100K population)').update_layout(layout)
 fig_new_cases_incidence.update_xaxes(showgrid=True, gridwidth=1, gridcolor=grid_color)
 fig_new_cases_incidence.update_yaxes(showgrid=True, gridwidth=1, gridcolor=grid_color)
-fig_new_cases_incidence.update_traces(line_color=line_color)
+fig_new_cases_incidence.update_traces(line_color=line_color, hovertemplate='%{x} <br><br>Daily Incidence Rate: %{y}')
 
 fig_new_cases_incidence_7_days = px.line(df, x='date', y='seven_days_incidence', labels={'date':'Date', 'seven_days_incidence':'Seven Day Incidence Rate'}, title='7 Days New Cases Incidence (New Cases per 100K population for the past 7 days)').update_layout(layout)
 fig_new_cases_incidence_7_days.update_xaxes(showgrid=True, gridwidth=1, gridcolor=grid_color)
 fig_new_cases_incidence_7_days.update_yaxes(showgrid=True, gridwidth=1, gridcolor=grid_color)
-fig_new_cases_incidence_7_days.update_traces(line_color=line_color)
+fig_new_cases_incidence_7_days.update_traces(line_color=line_color, hovertemplate='%{x} <br><br>Seven Day Incidence Rate: %{y}')
 
 app.layout = html.Div([
     # html.Br(),
@@ -131,11 +137,15 @@ app.layout = html.Div([
 
     html.Br(),
 
+    # html.Div(className='graphs_top', children=[
+    #     html.Div(children=[
+    #         dcc.Graph(id='new_cases_absolute', figure=fig_new_cases_absolute),
+    #         dcc.Graph(id='new_deaths', figure=fig_new_deaths),
+    #     ]),
+    # ]),
     html.Div(className='graphs_top', children=[
-        html.Div(children=[
-            dcc.Graph(id='new_cases_absolute', figure=fig_new_cases_absolute),
-            dcc.Graph(id='new_deaths', figure=fig_new_deaths),
-        ]),
+        dcc.Graph(id='new_cases_absolute', figure=fig_new_cases_absolute),
+        dcc.Graph(id='new_deaths', figure=fig_new_deaths),
     ]),
     html.Br(),
 
@@ -154,6 +164,16 @@ app.layout = html.Div([
             dcc.Graph(id='new_cases_incidence', figure=fig_new_cases_incidence)
         ])
     ]),
+
+    # html.Div(className='graphs',children=[dcc.Graph(id='new_cases_absolute_', figure=fig_new_cases_absolute)]),
+    # html.Div(className='graphs',children=[dcc.Graph(id='new_deaths_', figure=fig_new_deaths)]),
+    # html.Div(className='graphs',children=[dcc.Graph(id='total_cases_', figure=fig_total_cases)]),
+    # html.Div(className='graphs',children=[dcc.Graph(id='total_deaths_', figure=fig_total_deaths)]),
+    # html.Div(className='graphs',children=[dcc.Graph(id='fatality_', figure=fig_fatality)]),
+    # html.Div(className='graphs',children=[dcc.Graph(id='deaths_poll_', figure=fig_deaths_poll)]),
+    # html.Div(className='graphs',children=[dcc.Graph(id='new_cases_smoothed_', figure=fig_new_cases_smoothed)]),
+    # html.Div(className='graphs',children=[dcc.Graph(id='new_cases_incidence_', figure=fig_new_cases_incidence)]),
+
 
     html.Br(),
     html.H2('* Daily Incidence Rate: Number of new cases per 100k residents'),
